dacec56b6b4a07aa54ca97fc154087f4dd9d1f41
Andris Briedis <andris_b_s@inbox.lv>
2010-11-17 16:56:03 +0200

    added debug macro

--- src/CDebug.h
+++ src/CDebug.h
@@ -1,6 +1,15 @@
 #ifndef CDEBUG_H
 #define CDEBUG_H
 
+#ifdef CIVSIM_DBG
+	#define C_DBG(a, ...) printf("MEDIAPLUGIN (%s:%d): " a "\n", \
+                        __PRETTY_FUNCTION__, \
+	                        __LINE__, \
+	                        ##__VA_ARGS__)
+#else
+	#define C_DBG(a, ...)
+#endif
+
 class CDebug {
 public:
     static void dbg(char * format, ...);
