5a402bc6aa29c31e7360495721ed3cf4140c1012
Andris Briedis <andris_b_s@inbox.lv>
2010-11-18 17:03:30 +0200

    Initial CPoint class implementation

---
+++ src/CPoint.cpp
@@ -0,0 +1,46 @@
+#include <math.h>
+#include "CPoint.h"
+
+CPoint::CPoint() {
+	m_x = 0.0;
+	m_y = 0.0;
+}
+
+CPoint::CPoint(float x, float y) {
+	m_x = x;
+	m_y = y;
+}
+
+void CPoint::setX(float x) {
+	m_x = x;
+}
+
+void CPoint::setY(float y) {
+	m_y = y;
+}
+
+void CPoint::moveTo(float x, float y) {
+	m_x = x;
+	m_y = y;
+}
+
+void CPoint::move(float direction, float distance) {
+	float dx = cos(direction) * distance;
+	float dy = sin(direction) * distance;
+	m_x += dx;
+	m_y += dy;
+}
+
+float CPoint::x() const {
+	return m_x;
+}
+
+float CPoint::y() const {
+	return m_y;
+}
+
+float CPoint::distanceTo(const CPoint & other) const{
+	float dx = fabs(m_x - other.m_x);
+	float dy = fabs(m_y - other.m_y);
+	return sqrt(dx * dx + dy * dy);
+}
--- src/CPoint.h
+++ src/CPoint.h
@@ -8,7 +8,6 @@ public:
     /** @brief Constructor.
 
         Create point at 0;0 position.
-        @todo Implement.
     */
     CPoint();
     
@@ -17,19 +16,16 @@ public:
         @param y Y coordinate of the point.
         
         Create point at the given x, y position.
-        @todo Implement.
     */    
     CPoint(float x, float y);
     
     /** @brief Set x value of the point.
         @param x New x value.
-        @todo Implement.
     */
     void setX(float x);
     
     /** @brief Set y value of the point.
         @param y New y value.
-        @todo Implement.
     */        
     void setY(float y);
     
@@ -38,35 +34,34 @@ public:
         @param y New y value.
         
         This is equivalent to calling setX(); setY();.
-        @todo Implement.
     */
     void moveTo(float x, float y);
     
     /** @brief Move the point.
         @param direction Direction in radians.
         @param distance Distance to move.
-        @todo Implement.
     */
     void move(float direction, float distance);
 
     /** @brief Get current x value.
         @return X coordinate of the point.
-        @todo Implement.
     */
     float x() const;
     
     /** @brief Get current y value.
         @return Y coordinate of the point.
-        @todo Implement.
     */
     float y() const;
     
     /** @brief Distance from one point to another.
         @param other Point to measure distance to.
         @return Distance to the point.
-        @todo Implement.
     */
     float distanceTo(const CPoint & other) const;
+	
+private:
+	float m_x;
+	float m_y;
 };
 
 #endif //CPOINT_H
